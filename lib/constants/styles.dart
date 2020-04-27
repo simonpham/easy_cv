66f9ea9ffdc72c6b19f064771166dec99cc28653
Simon Pham <simonpham.dn@gmail.com>
2020-04-28 00:16:46 +0700

    feat: change theme colors

--- lib/constants/styles.dart
+++ lib/constants/styles.dart
@@ -3,21 +3,25 @@ import 'package:flutter/material.dart';
 final ThemeData appTheme = ThemeData(
   brightness: Brightness.light,
   scaffoldBackgroundColor: const Color.fromRGBO(250, 250, 250, 1.0),
-  primarySwatch: Colors.indigo,
+  primarySwatch: Colors.blue,
+  primaryColor: Color(0xff304080),
   inputDecorationTheme: inputDecorationTheme,
   cardTheme: cardTheme,
   buttonTheme: buttonTheme,
   cursorColor: Colors.indigo,
+  accentColor: Colors.green,
 );
 
 final ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
   scaffoldBackgroundColor: Colors.black,
-  primarySwatch: Colors.indigo,
+  primarySwatch: Colors.blue,
+  primaryColor: Color(0xff304080),
   inputDecorationTheme: inputDecorationTheme,
   cardTheme: cardTheme.copyWith(color: Colors.black),
   buttonTheme: buttonTheme,
   cursorColor: Colors.indigo,
+  accentColor: Colors.green,
 );
 
 final inputDecorationTheme = InputDecorationTheme(
