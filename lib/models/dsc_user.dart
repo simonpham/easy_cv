fd8f77259e9c30ab166194f4ae1c9a8c1c11c0c3
Simon Pham <simonpham.dn@gmail.com>
2020-04-27 20:30:48 +0700

    feat: update user model

--- lib/models/dsc_user.dart
+++ lib/models/dsc_user.dart
@@ -1,26 +1,43 @@
 class DscUser {
   String uid;
   String email;
-  String name;
+  String firstName;
+  String lastName;
   String username;
   String gender;
   String profilePicUrl;
+  String intro;
+  String bio;
+  String location;
+  String github;
+  String website;
 
   DscUser({
     this.uid,
     this.email,
-    this.name,
+    this.firstName,
     this.username,
     this.gender,
     this.profilePicUrl,
+    this.intro,
+    this.bio,
+    this.location,
+    this.github,
+    this.website,
   });
 
   DscUser.fromMap(Map<String, dynamic> map) {
     uid = map['uid'];
     email = map['email'];
-    name = map['name'];
+    firstName = map['first_name'];
+    lastName = map['last_name'];
     username = map['username'];
     gender = map['gender'];
     profilePicUrl = map['profile_pic_url'];
+    intro = map['intro'];
+    bio = map['bio'];
+    location = map['location'];
+    github = map['github'];
+    website = map['website'];
   }
 }
