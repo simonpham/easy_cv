a238eec8a1c9f1f31be0808024078f0ed2296276
Simon Pham <simonpham.dn@gmail.com>
2020-04-28 00:18:20 +0700

    feat: add experience and education to profile view model

--- lib/view_models/profile_view_model.dart
+++ lib/view_models/profile_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:easy_cv/models/dsc_user.dart';
+import 'package:easy_cv/models/story.dart';
 import 'package:easy_cv/services/dsc_portal_api_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -17,6 +18,24 @@ class ProfileViewModel extends Model {
     notifyListeners();
   }
 
+  List<Story> _experience;
+
+  List<Story> get experience => _experience;
+
+  set experience(List<Story> value) {
+    _experience = value;
+    notifyListeners();
+  }
+
+  List<Story> _education;
+
+  List<Story> get education => _education;
+
+  set education(List<Story> value) {
+    _education = value;
+    notifyListeners();
+  }
+
   Future<bool> loadProfile([String username]) async {
     try {
       if (username != null) {
@@ -27,5 +46,29 @@ class ProfileViewModel extends Model {
       user = null;
     }
     return user != null;
+  }
+
+  Future<bool> loadExperience([String uid]) async {
+    try {
+      final _uid = uid ?? user.uid;
+      if (_uid != null) {
+        experience = await apiSvc?.getUserExperience(_uid);
+      }
+    } catch (error) {
+      experience = null;
+    }
+    return experience != null;
+  }
+
+  Future<bool> loadEducation([String uid]) async {
+    try {
+      final _uid = uid ?? user.uid;
+      if (_uid != null) {
+        education = await apiSvc?.getUserEducation(_uid);
+      }
+    } catch (error) {
+      education = null;
+    }
+    return education != null;
   }
 }
